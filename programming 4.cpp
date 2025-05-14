b340c86e774275f25cc7ca10a2cfc488182c6e9a
mohammadhammad12 <niazihammad1234@gmail.com>
2025-05-14 09:39:28 +0500

    Add files via upload

---
+++ programming 4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace  std;
+int main()
+{ int length=3;
+int array[length];
+for(int i=0;i<length;i++)
+{cout<<"enter the value ";
+cin>>array[length];
+}
+for(int j=0;j<length;j++)
+{cout<<"value is "<<j<<"index is "<<array[j]<<endl;
+}
+return 0;
+}
